4982e2dfda70c2e2fcaaa3f0b31d0f4c81097420
jivishwa <jivishwa@cisco.com>
2024-07-26 16:32:20 +0530

    All module

---
+++ module-a/src/main/java/com/jitendra/modulea/ModuleAApp.java
@@ -0,0 +1,12 @@
+package com.jitendra.modulea;
+
+
+/**
+ *  This Test Java Docs for Module A
+ */
+public class ModuleAApp {
+
+    public String hello(){
+        return "Hello Module A";
+    }
+}
---
+++ module-b/src/main/java/com/jitendra/moduleb/ModuleBApp.java
@@ -0,0 +1,12 @@
+package com.jitendra.moduleb;
+
+/**
+ *  This is Test Java Doc for Module B
+ */
+public class ModuleBApp {
+
+
+    public String hello(){
+        return "Hello Module B";
+    }
+}
